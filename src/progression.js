ae70e1712d70fc24aeba06b90a459c19ea5b68da
Sergey Syromyatnikov <sergsyrom@gmail.com>
2023-01-15 21:56:54 +0500

    add: "lib.js"

--- src/progression.js
+++ src/progression.js
@@ -1,21 +1,9 @@
 import readlineSync from 'readline-sync';
-
-const getUserName = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
-  return userName;
-};
+import { getUserName, getRandomNumber } from './lib.js';
 
 const getUserAnswer = () => {
   const userAnswer = readlineSync.question('Your answer: ');
   return userAnswer;
-};
-
-const getRandomNumber = () => {
-  const min = 1;
-  const max = 9;
-  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 const getProgression = () => {
@@ -36,7 +24,7 @@ const getProgression = () => {
 const getQuestion = () => {
   console.log('What number is missing in the progression?');
   const arr = getProgression();
-  const item = Math.floor(Math.random() * (arr.length - 1));
+  const item = getRandomNumber();
   const controlResult = arr[item];
   arr[item] = '..';
   const strArr = arr.join(' ');
